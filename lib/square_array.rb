893965d22233c474f3a488724b94e0d816043a29
Chandon Porter <cneilporter94@gmail.com>
2019-07-24 11:55:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,4 +1,10 @@
-
+def square_array(numbers)
+  new_numbers = []
+  numbers.each do |num|
+    new_numbers << (num ** 2)
+  end
+  return new_numbers
+end
 
 def square_array(numbers)
  new_numbers = numbers.collect{|num| num ** 2}
